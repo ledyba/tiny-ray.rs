2cf78403a8b0c60cd70345645ccfc310d1dfb0e8
Kaede Fujisaki <kaede@hexe.net>
2022-08-21 02:43:18 +0900

    init

---
+++ README.md
@@ -0,0 +1,11 @@
+# Tiny ray-tracing
+
+```shell
+cargo run
+```
+
+# References
+
+- [週末レイトレーシング (翻訳)](https://inzkyk.xyz/ray_tracing_in_one_weekend/)
+- [週末レイトレーシング - 達人出版会](https://tatsu-zine.com/books/ray-tracing-part1)
+g
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,25 @@
+use log::info;
+
+fn setup_logger(level: log::LevelFilter) -> Result<(), fern::InitError> {
+  fern::Dispatch::new()
+    .format(|out, message, record| {
+      out.finish(format_args!(
+        "{}[{}][{}] {}",
+        chrono::Local::now().format("[%Y-%m-%d][%H:%M:%S]"),
+        record.target(),
+        record.level(),
+        message
+      ))
+    })
+    .level(level)
+    .chain(std::io::stdout())
+    .chain(fern::log_file("output.log")?)
+    .apply()?;
+  Ok(())
+}
 
 fn main() -> anyhow::Result<()> {
+  setup_logger(log::LevelFilter::Info)?;
+  info!("Initialized.");
   Ok(())
 }
