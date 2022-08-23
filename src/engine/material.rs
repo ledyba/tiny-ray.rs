3a234df8114803cf479cc570de0124fa328820d4
Kaede Fujisaki <kaede@hexe.net>
2022-08-24 02:16:35 +0900

    trait

--- src/engine/material.rs
+++ src/engine/material.rs
@@ -1,3 +1,16 @@
-pub trait Material {
+use palette::LinSrgb;
+use crate::engine::hittable::HitRecord;
+use crate::engine::ray::Ray;
+use crate::math::Vec3;
 
+pub enum Response {
+  Reflection {
+    scattering: Vec3,
+    attenuation: LinSrgb,
+  },
+  Absorption,
+}
+
+pub trait Material {
+  fn hit(ray: &Ray, hit: HitRecord) -> Response;
 }
