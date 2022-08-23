0888e21c12d4a8d40f6689de04991ab69e37c7b5
Kaede Fujisaki <kaede@hexe.net>
2022-08-23 20:47:17 +0900

    sky

--- src/engine.rs
+++ src/engine.rs
@@ -64,7 +64,7 @@ impl Engine {
   }
   fn sky(&self, ray: &Ray) -> LinSrgb {
     let t = 0.5 * (ray.direction().normalized().y + 1.0);
-    let blue = LinSrgb::new(0.5, 0.7, 1.0);
+    let blue = LinSrgb::new(0.25, 0.5, 1.0);
     LinSrgb::new(1.0, 1.0, 1.0).mix(&blue, t.sqrt())
   }
 }
