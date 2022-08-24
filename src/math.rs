0edf082089639851c1d8721f35fd9d9f25d27bff
Kaede Fujisaki <kaede@hexe.net>
2022-08-24 19:31:21 +0900

    Use more clever way

--- src/math.rs
+++ src/math.rs
@@ -149,9 +149,15 @@ impl std::ops::DivAssign<f32> for Vec3 {
 }
 
 pub fn random_direction(len: f32) -> Vec3 {
-  len * Vec3::new(
-    rand::random::<f32>() - 0.5,
-    rand::random::<f32>() - 0.5,
-    rand::random::<f32>() - 0.5
-  ).normalized()
+  let z = f32::min((rand::random::<f32>() - 0.5) * 2.0, 1.0);
+  let r = (1.0 - z * z).sqrt();
+  let theta = (rand::random::<f32>() * 360.0).to_radians();
+  let cos = theta.cos();
+  let sin = theta.sin();
+  let n = Vec3::new(
+    r * cos,
+    r * sin,
+    z,
+  );
+  len * n
 }
