b122057353a8994a16641396f282b5acbead5ae1
Kaede Fujisaki <kaede@hexe.net>
2022-08-28 03:48:58 +0900

    todo

--- src/render/entity/volume_tree/builder.rs
+++ src/render/entity/volume_tree/builder.rs
@@ -1,5 +1,26 @@
 use crate::render::Entity;
+use crate::render::entity::collection::EntityCollection;
 
 pub struct VolumeTreeBuilder {
   entities: Vec<Box<dyn Entity>>,
+}
+
+impl VolumeTreeBuilder {
+  pub fn new() -> Self {
+    Self {
+      entities: Vec::new(),
+    }
+  }
+  pub fn push(&mut self, entity: impl Entity + 'static) {
+    self.entities.push(Box::new(entity));
+  }
+  pub fn build(mut self) -> Box<dyn Entity> {
+    if self.entities.is_empty() {
+      return Box::new(EntityCollection::new());
+    }
+    if self.entities.len() == 1 {
+      return self.entities.remove(0);
+    }
+    todo!()
+  }
 }
