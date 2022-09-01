1ff9aab41c493e5f69445d7b6569d1b2bdfcb44c
Kaede Fujisaki <kaede@hexe.net>
2022-09-01 23:41:39 +0900

    normalize everytime

--- src/util/math/quaternion.rs
+++ src/util/math/quaternion.rs
@@ -45,17 +45,27 @@ impl Quaternion {
       w: self.w,
     }
   }
+  fn normalized(&self) -> Self {
+    let length = (self.x * self.x + self.y * self.y + self.z * self.z * self.w * self.w).sqrt();
+    Self {
+      x: self.x / length,
+      y: self.y / length,
+      z: self.z / length,
+      w: self.w / length,
+    }
+  }
 }
 
 impl std::ops::Mul for Quaternion {
   type Output = Quaternion;
   fn mul(self, rhs: Self) -> Self::Output {
-    Self {
+    let m = Self {
       x: (self.x * rhs.w) + (self.y * rhs.z) - (self.z * rhs.y) + (self.w * rhs.x),
       y: -(self.x * rhs.z) + (self.y * rhs.w) + (self.z * rhs.x) + (self.w * rhs.y),
       z: (self.x * rhs.y) - (self.y * rhs.x) + (self.z * rhs.w) + (self.w * rhs.z),
       w: -(self.x * rhs.x) - (self.y * rhs.y) - (self.z * rhs.z) + (self.w * rhs.w),
-    }
+    };
+    m.normalized()
   }
 }
 
@@ -75,8 +85,8 @@ mod test {
   fn inverse() {
     let v = Vec3::new(1.0, 0.0, 0.0);
     let q = Quaternion::from_angle_axis(90.0, Vec3::new(0.0, 0.0, 1.0));
-    let r = q.rotate(v).normalized(); // FIXME: Float....
-    let r = q.inverse().rotate(r).normalized(); // FIXME: Float....
+    let r = q.rotate(v);
+    let r = q.inverse().rotate(r);
     assert_eq!(Vec3::new(1.0, 0.0, 0.0), r);
   }
 }
