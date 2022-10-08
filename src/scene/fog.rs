1c574c51af4f7dd035c67ab3ca1139da0ba6656d
Kaede Fujisaki <kaede@hexe.net>
2022-10-08 19:07:53 +0900

    Update scene

--- src/scene/fog.rs
+++ src/scene/fog.rs
@@ -11,15 +11,16 @@ pub fn fog(canvas: &Image) -> Scene {
   use material::Material;;
   let mut scene = Scene::new();
   let white_material: Arc<dyn Material> = Arc::new(material::Lambert::new(LinSrgb::new(0.8, 0.8, 0.8)));
-  let emission_material: Arc<dyn Material> = Arc::new(material::DiffuseLight::new(LinSrgb::new(1.0 * 25.0, 0.65 * 25.0, 0.0 * 25.0)));
+  let metal_material: Arc<dyn Material> = Arc::new(material::Metal::new(LinSrgb::new(1.0, 0.3, 0.3), 0.0));
+  let emission_material: Arc<dyn Material> = Arc::new(material::DiffuseLight::new(LinSrgb::new(0.99 * 30.0, 0.84 * 30.0, 0.0 * 30.0)));
 
   //scene.sky_box(sky_box::BlueSky::new());
   scene.push(entity::Plane::new(Vec3::zero(), 1000.0, 1000.0, Arc::clone(&white_material)));
   scene.push(
     entity::Sphere::new(
-      Vec3::new(-3.0, 1.0, -2.0),
+      Vec3::new(-3.0, 1.0, -3.0),
       1.0,
-      Arc::clone(&white_material),
+      Arc::clone(&metal_material),
     ));
   scene.push(entity::Cuboid::new(Vec3::new(0.0, 0.5, 0.0), 1.0, 1.0, 1.0, Arc::clone(&white_material)));
   scene.push(
@@ -33,7 +34,7 @@ pub fn fog(canvas: &Image) -> Scene {
     ));
   scene.push(
     entity::Sphere::new(
-      Vec3::new(1.5, 3.0, -1.0),
+      Vec3::new(1.0, 3.0, -2.5),
       0.5,
       Arc::clone(&emission_material),
     ));
