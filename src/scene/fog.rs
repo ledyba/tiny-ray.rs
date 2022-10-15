f0066e3f96318c3afa968642d457dc864332060a
Kaede Fujisaki <kaede@hexe.net>
2022-10-15 09:03:31 +0900

    Fix scene

--- src/scene/fog.rs
+++ src/scene/fog.rs
@@ -10,15 +10,14 @@ pub fn fog(canvas: &Image) -> Scene {
   use material::Material;
   let mut scene = Scene::new();
   let white_material: Arc<dyn Material> = Arc::new(material::Lambert::new(LinSrgb::new(0.8, 0.8, 0.8)));
-  let metal_material: Arc<dyn Material> = Arc::new(material::Metal::new(LinSrgb::new(1.0, 0.3, 0.3), 0.0));
-  let emission_material: Arc<dyn Material> = Arc::new(material::DiffuseLight::new(LinSrgb::new(0.99 * 30.0, 0.84 * 30.0, 0.0 * 30.0)));
+  let emission_material: Arc<dyn Material> = Arc::new(material::DiffuseLight::new(LinSrgb::new(30.0, 30.0, 30.0)));
 
   scene.push(entity::Plane::new(Vec3::zero(), 1000.0, 1000.0, Arc::clone(&white_material)));
   scene.push(
     entity::Sphere::new(
       Vec3::new(-5.0, 1.0, -5.0),
       1.0,
-      Arc::clone(&metal_material),
+      Arc::clone(&white_material),
     ));
   scene.push(entity::Cuboid::new(
     Vec3::new(0.8, 0.5, 0.4),
@@ -32,7 +31,7 @@ pub fn fog(canvas: &Image) -> Scene {
         100.0, 100.0, 100.0,
         Arc::clone(&white_material)),
       0.5,
-      LinSrgb::new(0.8, 0.8, 0.8),
+      LinSrgb::new(0.5, 0.5, 0.5),
     ));
   scene.push(
     entity::Sphere::new(
